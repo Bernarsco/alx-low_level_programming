a6f0f27dcff8f95603a45c2e6aba860e484704d3
Bernarsco <bernardtetteh367@gmail.com>
2023-03-31 16:02:06 -0700

    copy a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
- * _strncpy - strings to be copied
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
- * return: dest
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
  */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
